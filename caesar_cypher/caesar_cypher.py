b54e388fcc99efa87b377dac691625486a3371c3
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-10-14 15:03:50 +0530

    using try-except

---
+++ caesar_cypher/caesar_cypher.py
@@ -0,0 +1,27 @@
+import string
+import art
+
+logo = art.logo
+alphabet = string.ascii_lowercase
+alphabet = list(alphabet)
+
+print(logo)
+
+operation = input("Choose the operation 'Encode/decode': ")
+original_text = input("Enter the text: ")
+shift = int(input("Enter shift amount: "))
+
+def caesar (original_text, shift, operation):
+    text = ""
+    if operation.startswith('d'):
+       shift *= -1
+    for letter in original_text:
+        try:
+            shifted_amount = alphabet.index(letter) + shift
+            shifted_amount %= len(alphabet)
+            text += alphabet[shifted_amount]
+        except:
+            text += letter
+    print(text)
+
+caesar(original_text, shift, operation)
