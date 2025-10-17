b9c3cdf838b98c9f35bd4d131f58a93fa892f8f5
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-10-17 23:46:51 +0530

    working game

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -10,51 +10,99 @@ def deal_card():
         user_cards.append(random.choice(cards))
         computer_cards.append(random.choice(cards))
 
+def compare_hands():
+    global game_end
+    user = sum(user_cards)
+    comp = sum(computer_cards)
+    
+    if is_blackjack(computer_cards):
+        show_real_hand()
+        print("You loose.")
+        game_end = True
+
+    elif user > comp:
+        show_real_hand()
+        print("You win")
+        game_end = True
+    elif user < comp:
+        show_real_hand()
+        print("You loose")
+        game_end = True
+    else:
+        show_real_hand()
+        print("Draw")
+        game_end = True
+    
+
 def comp_plays():
+    global game_end
     while sum(computer_cards) < 17:
         computer_cards.append(random.choice(cards))
+        if is_bust(computer_cards):
+            show_real_hand()
+            print("You win")
+            game_end = True
+            break
+    if not game_end:
+        compare_hands()
 
 
 
 def get_another_card():
+    global game_end
     hit = input("Do you want to draw another card (Y/n): ").lower().strip()
     if hit == 'y':
         user_cards.append(random.choice(cards))
-        is_blackjack()
+        if is_bust(user_cards):
+            show_real_hand()
+            print("You loose.")
+            game_end = True
     else:
         comp_plays()
 
+def show_current_hand():
+    print(F"Your hand: {user_cards} ({sum(user_cards)})")
+    print(f"Computer's hand: [{computer_cards[0]}, ?]")
 
-def is_blackjack():
-    user = sum(user_cards)
-    comp = sum(computer_cards)
+def show_real_hand():
+    print(F"Your hand: {user_cards} ({sum(user_cards)})")
+    print(f"Computer's hand: {computer_cards} ({sum(computer_cards)})")
 
-    if user == 21:
-        print("You win")
-    elif comp == 21:
-        print("You loose")
-    elif user > 21:
-        if 11 in user_cards:
-            index = user_cards.index(11)
-            user_cards[index] = 1
-            if user > 21:
-                print("You loose")
-            else:
-                get_another_card()
-        else:        
-            print("You loose")
+def is_blackjack(check_cards):
+    if sum(check_cards) == 21:
+        return True
     else:
-        get_another_card()
-        if comp > 21:
-            print("You loose")
-        else:
-            if user < comp:
-                print("You loose")
-            elif user > comp:
-                print("You win")
+        return False
+    
+
+def is_bust(check_cards):
+    if sum(check_cards) > 21:
+        if 11 in check_cards:
+            index = check_cards.index(11)
+            check_cards[index] = 1
+            if sum(check_cards) > 21:
+                return True
             else:
-                print("Draw")
+                return False
+        else:
+            return True
+    else:
+        return False
 
 
 deal_card()
-is_blackjack()
+
+
+game_end = False
+
+
+while not game_end:
+    show_current_hand()
+
+    if is_blackjack(user_cards):
+        show_real_hand()
+        print("Blackjack!, You win.")
+        game_end = True
+    
+    else:
+        get_another_card()
