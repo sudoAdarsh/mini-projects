bbcb299edc458ea69f96e7ac20580de2727bc373
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-10-31 22:02:25 +0530

    Added controls for snake

--- snakes/main.py
+++ snakes/main.py
@@ -1,17 +1,30 @@
 from turtle import Turtle, Screen
+from snake import Snake
+import time
+
 
 my_screen = Screen()
 my_screen.setup(width=600, height=600)
 my_screen.bgcolor("black")
 my_screen.title("Snake game")
+my_screen.tracer(0)
 
-big_snake = []
-starting_posistions = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
 
-for position in starting_posistions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.goto(position)
-    big_snake.append(new_segment) 
+my_screen.listen()
+
+my_screen.onkey(key="Up", fun=snake.up)
+my_screen.onkey(key="Down", fun=snake.down)
+my_screen.onkey(key="Left", fun=snake.left)
+my_screen.onkey(key="Right", fun=snake.right)
+
+game_is_on = True
+
+while game_is_on:
+    my_screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
 
 my_screen.exitonclick()
---
+++ snakes/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.main_snake = []
+        self.create_snake()
+        self.snake_head = self.main_snake[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")
+            new_segment.penup()
+            new_segment.color("white")
+            new_segment.goto(position)              
+            self.main_snake.append(new_segment) 
+
+    def move(self):
+        for segment in range(len(self.main_snake) - 1, 0, -1):
+            new_x = self.main_snake[segment - 1].xcor()
+            new_y = self.main_snake[segment - 1].ycor()
+            self.main_snake[segment].goto(new_x, new_y)
+        self.snake_head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
+
+    def down(self):
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
+
+    def left(self):
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
+
+    def right(self):
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
