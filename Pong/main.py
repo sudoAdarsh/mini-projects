0e8aed4356fa3ad32e515e1274d77030a3da2c39
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-11-04 22:23:44 +0530

    Added balls

--- Pong/main.py
+++ Pong/main.py
@@ -1,32 +1,43 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 my_screen = Screen()
 my_screen.title("Pong")
 my_screen.setup(height=600, width=800)
 my_screen.bgcolor("black")
+my_screen.tracer(0) 
 
-paddle_position = [(350, 0), (-350, 0)]
-
-paddles = []
-
-for paddle in paddle_position:
-    new_paddle = Paddle()
-    new_paddle.goto(paddle)
-    new_paddle.create_paddle()
-    paddles.append(new_paddle)
-
-left_paddle = paddles[1]
-right_paddle = paddles[0]
+left_paddle = Paddle((-350, 0))
+right_paddle = Paddle((350, 0))
+ball = Ball()
 
 my_screen.listen()
-
 my_screen.onkey(key="Up", fun= right_paddle.up)
 my_screen.onkey(key="Down", fun= right_paddle.down)
-
-
 my_screen.onkey(key="w", fun= left_paddle.up)
 my_screen.onkey(key="s", fun= left_paddle.down)
 
+
+
+game_is_on = True
+
+while game_is_on:
+    my_screen.update()
+    time.sleep(0.05)
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if (left_paddle.distance(ball) < 50 and ball.xcor() > -320) or (right_paddle.distance(ball) < 50 and ball.xcor() > 320):
+        ball.bounce_x()  
+
+    if ball.xcor() > 380: 
+        ball.restart()
+    
+    if ball.xcor() < -380:
+        ball.restart()
 
 my_screen.exitonclick()
--- Pong/paddle.py
+++ Pong/paddle.py
@@ -1,21 +1,22 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, cords):
         super().__init__()
+        self.cords = cords
+        self.create_paddle()
 
     def create_paddle(self):
         self.shape("square")
         self.color("white")
-        self.shapesize(stretch_wid=10, stretch_len=2)
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
+        self.goto(self.cords)
 
     def up(self):
-        x_cord = self.xcor()
         y_cord = self.ycor()
-        self.goto(x_cord, y_cord + 20)
+        self.goto(self.xcor(), y_cord + 30)
 
     def down(self):
-        x_cord = self.xcor()
         y_cord = self.ycor()
-        self.goto(x_cord, y_cord - 20)
+        self.goto(self.xcor(), y_cord - 30)
