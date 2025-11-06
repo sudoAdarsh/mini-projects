ee3d700a590c67e3cd0db16b5666354d161e4527
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-11-06 13:03:09 +0530

    Builded basic game still have some bugs

--- cross_the_road/main.py
+++ cross_the_road/main.py
@@ -8,7 +8,33 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+timmy = Player()
+score = Scoreboard()
+car_manager = CarManager()
+
+
+screen.listen()
+
+screen.onkey(fun=timmy.move, key="Up")
+
 game_is_on = True
 while game_is_on:
+
+    # start spawing car
+    car_manager.spawn_cars()
+    car_manager.move_cars()
+    # check timmy reached next level
+    if timmy.ycor() > 280:
+        timmy.next_level()
+        score.update_scoreboard()
+        car_manager.increase_speed()
+    
+    for _ in range(len(car_manager.all_cars)):
+        if timmy.distance(car_manager.all_cars[_]) < 30:
+            game_is_on = False
+            score.game_over()
+
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
--- cross_the_road/scoreboard.py
+++ cross_the_road/scoreboard.py
@@ -1,5 +1,22 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+FONT = ("CourierPrime", 20, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(-230, 260)
+        self.color("black")
+        self.level = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.level += 1 
+        self.write(arg=f"Level: {self.level}", align="center", font=FONT)
+
+    def game_over(self):
+        self.home()
+        self.write(arg="GAME OVER", align="center", font=FONT)
