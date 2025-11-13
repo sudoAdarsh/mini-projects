c011f8a6439b300231272d81cdb6383722a443b2
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-11-13 16:53:40 +0530

    updates in pandas indexing (.iloc[])

--- nato_alphabets/main.py
+++ nato_alphabets/main.py
@@ -3,7 +3,7 @@ import pandas
 #TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-alphabets_dict = {row.iloc[0] : row.iloc[1] for (index, row) in data.iterrows()}
+alphabets_dict = {row.letter : row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
