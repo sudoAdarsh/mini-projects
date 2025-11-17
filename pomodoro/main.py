a9d0b4a54b4d0fa233bf9e7f80e63b9ec68a87fd
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-11-17 20:17:22 +0530

    Got a GUI using tkinter

---
+++ pomodoro/main.py
@@ -0,0 +1,42 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier Prime"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+label = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+label.grid(row=0, column=1)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_image = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+canvas.grid(row=1, column=1)
+
+
+button1 = Button(text="Start", highlightthickness=0)
+button1.grid(row=2, column=0)
+
+button2 = Button(text="Reset", highlightthickness=0)
+button2.grid(row=2, column=2)
+
+label2 = Label(text="✔", font=(FONT_NAME, 25), bg=YELLOW, fg=GREEN)
+label2.grid(row=3, column=1)
+
+window.mainloop()
