ccd3a2ffb8fb540b23548010d81c0641d7fb5b77
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-11-18 09:08:28 +0530

    Start timer feature

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -8,13 +9,36 @@ FONT_NAME = "Courier Prime"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN
+    long_break_sec = LONG_BREAK_MIN
 
+    if reps % 8 == 0:
+        count = long_break_sec
+    elif reps % 2 == 0:
+        count = short_break_sec
+    else:
+        count = work_sec
+    
+    count_down(count)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_minute = math.floor(count / 60)
+    count_seconds = count % 60
 
+    canvas.itemconfig(timer_text, text=f"{count_minute:02}:{count_seconds:02}")
+    if count >= 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
@@ -26,11 +50,11 @@ label.grid(row=0, column=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
 canvas.grid(row=1, column=1)
 
 
-button1 = Button(text="Start", highlightthickness=0)
+button1 = Button(text="Start", highlightthickness=0, command=start_timer)
 button1.grid(row=2, column=0)
 
 button2 = Button(text="Reset", highlightthickness=0)
