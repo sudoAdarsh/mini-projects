a31833c1ddefe303e696cb298f8c04d1dc37653d
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-11-19 23:33:27 +0530

    This works the best

--- password_manager/main.py
+++ password_manager/main.py
@@ -9,30 +9,39 @@ FONT = ("Noto Sans", 13)
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=50, pady=50)
+window.config(padx=20, pady=20)
 
 canvas = Canvas(width=360, height=360)
 logo_image = PhotoImage(file="logo.png")
 canvas.create_image(180, 180, image=logo_image)
-canvas.grid(row=0, column=1)
+canvas.grid(row=0, column=0, columnspan=3)
+
+
+# Labels
 
 website_label = Label(text="Website:", font=FONT)
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=35, font=FONT)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="ew")
-
 email_label = Label(text="Email/Username:", font=FONT)
 email_label.grid(row=2, column=0)
-
-email_entry = Entry(width=35, font=FONT)
-email_entry.grid(row=2, column=1, columnspan=2, sticky="ew")
 
 password_label = Label(text="Password:", font=FONT)
 password_label.grid(row=3, column=0)
 
+
+# Entries
+
+website_entry = Entry(width=35, font=FONT)
+website_entry.grid(row=1, column=1, columnspan=2, sticky="ew")
+
+email_entry = Entry(width=35, font=FONT)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="ew")
+
 password_entry = Entry(width=21, font=FONT)
 password_entry.grid(row=3, column=1, sticky="ew")
+
+
+# Buttons
 
 generate_password_button = Button(text="Generate Password", font=FONT)
 generate_password_button.grid(row=3, column=2)
