e8df3bf582913c4783bbe64dbfeaa91547a2bad9
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-11-21 13:57:37 +0530

    Added Error Handling

--- nato_alphabets/main.py
+++ nato_alphabets/main.py
@@ -7,7 +7,13 @@ alphabets_dict = {row.letter : row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper().strip()
-word_list = list(word)
-word_nato_list = [alphabets_dict[letter] for letter in word_list]
-print(word_nato_list)
+while True:
+    try:
+        word = input("Enter a word: ").upper().strip()
+        word_list = list(word)
+        word_nato_list = [alphabets_dict[letter] for letter in word_list]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(word_nato_list)
+        break
