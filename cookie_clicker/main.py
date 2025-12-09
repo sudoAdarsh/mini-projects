bbe9c38014bd4483b490bfab270d517246dca72a
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-12-09 12:14:01 +0530

    auto-clicker done

--- cookie_clicker/main.py
+++ cookie_clicker/main.py
@@ -1,6 +1,22 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Firefox()
 driver.get("https://ozh.github.io/cookieclicker/")
+
+
+time.sleep(5)
+try:
+    driver.find_element(By.ID, value="langSelect-EN").click()
+except:
+    pass
+
+time.sleep(3)
+
+cookie = driver.find_element(By.ID, value="bigCookie")
+
+
+while True:
+    cookie.click()
