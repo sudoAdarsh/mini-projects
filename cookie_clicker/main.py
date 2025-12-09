e85bdc9c1045ef5d137e007369d6732fa20ffd9a
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-12-09 12:49:31 +0530

    Automated buying

--- cookie_clicker/main.py
+++ cookie_clicker/main.py
@@ -17,6 +17,17 @@ time.sleep(3)
 
 cookie = driver.find_element(By.ID, value="bigCookie")
 
-
+start_time = time.time()
+wait_time = 2
 while True:
     cookie.click()
+    current_time = time.time()
+    if (current_time - start_time) >= wait_time:
+        products = driver.find_elements(By.CSS_SELECTOR, value=".enabled")
+        max_product = products[-1]
+        max_product_price = int(max_product.text.split('\n')[1])
+        num_of_cookies = int(driver.find_element(By.ID, value="cookies").text.split()[0])
+        if num_of_cookies >= max_product_price:
+            max_product.click()
+            wait_time += 1
+        start_time = current_time
