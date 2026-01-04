803576579b2e1f7b633e9dba27e8ee24a40e923b
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2026-01-04 20:16:36 +0530

    Added all blogs on home route

--- blog_site/main.py
+++ blog_site/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
-
+from post import Post
 
 app = Flask(__name__)
+post = Post()
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return post.home()
+
+@app.route('/post/<int:blog_id>')
+def get_post(blog_id):
+    return post.render(blog_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- blog_site/post.py
+++ blog_site/post.py
@@ -1,2 +1,18 @@
+import requests
+from flask import render_template
+
 class Post:
-    pass
+    def __init__(self):
+        self.blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+        self.blog_data = self.all_posts()
+
+    def all_posts(self):
+        return requests.get(self.blog_url).json()
+
+    def home(self):
+        return render_template("index.html", blogs=self.blog_data)
+    
+    def render(self, blog_id):
+        for blog in self.blog_data:
+            if blog["id"] == blog_id:
+                return render_template("post.html", **blog)
